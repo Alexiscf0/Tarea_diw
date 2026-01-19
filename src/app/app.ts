a70a90b5b172ac4695fb4fb07b28659ad93b4af1
Alexis Castellón <acf0037@alu.medac.es>
2026-01-19 12:43:58 +0100

    Tarea de Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,24 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ejercicio2');
+  contador = 0;
+incrementar() {
+  this.contador += 1;
 }
+
+restar() {
+  this.contador -= 1;
+}
+resetear() {
+  this.contador = 0;
+}
+ 
+}
+
+ 
